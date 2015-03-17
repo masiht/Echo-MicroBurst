71f0dbce6d700eb1c030cf70292ca4eaa03d40df
Masih Tabrizi <masih_tabrizi@softwaremerchant.com>
2015-03-17 10:10:38 -0400

    Initial Commit

---
+++ MB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MB
+//
+//  Created by Masih Tabrizi on 3/17/15.
+//  Copyright (c) 2015 Masih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MB
+//
+//  Created by Masih Tabrizi on 3/17/15.
+//  Copyright (c) 2015 Masih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
