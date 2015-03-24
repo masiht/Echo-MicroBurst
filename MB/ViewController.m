4908b1cb228f72613cab6bfedcd4ab3e0861e861
Masih Tabrizi <masih_tabrizi@softwaremerchant.com>
2015-03-23 23:02:54 -0400

    Upload test finished.

--- MB/ViewController.m
+++ MB/ViewController.m
@@ -9,24 +9,24 @@
 #import "ViewController.h"
 
 #define serverIP @"108.229.9.53"
-#define serverCtrlProt 62122
+#define serverCtrlPort 62122
 #define serverUploadPort 62123
 
 
 @interface ViewController () {
     
     GCDAsyncSocket *socketCtrl;
-    GCDAsyncUdpSocket *socketUplaod;
+    GCDAsyncUdpSocket *socketUpload;
 
     int numberOfTestPackets;
     int sizeOfTestPackets;
-    BOOL isWatingForResults;
+    BOOL isWaitingForResults;
     
     testType currentTest;
     NSArray *dataToSend;
-    NSMutableArray *recvTimeStamp;
+    NSMutableArray *recvTimeStamps;
     NSMutableArray *recvData;
-    NSDate *startDate;
+    NSDate *startTime;
 }
 
 @end
@@ -37,11 +37,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    isWatingForResults = NO;
+    isWaitingForResults = NO;
     currentTest = testTypeUpload;
     
     if (currentTest == testTypeUpload) {
-//        [self setupUploadTest];
+        [self setupUploadTest];
         [self setupControlConnection];
     }
     
@@ -55,12 +55,12 @@
 -(void)setupControlConnection {
     socketCtrl = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
     NSError *error;
-    [socketCtrl connectToHost:serverIP onPort:serverCtrlProt error:&error];
+    [socketCtrl connectToHost:serverIP onPort:serverCtrlPort error:&error];
     
 }
 
 -(void)setupUploadTest {
-    socketUplaod = [[GCDAsyncUdpSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+    socketUpload = [[GCDAsyncUdpSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
     
 }
 
@@ -69,30 +69,44 @@
     numberOfTestPackets = numberOfPackets;
     sizeOfTestPackets = size;
     
-    switch (currentTest) {
-            
-        case testTypeUpload:
-            return [[NSString stringWithFormat:@"0:1:%i:%i:%@", numberOfPackets, size, [socketCtrl localHost]] dataUsingEncoding:NSUTF8StringEncoding];;
-            break;
-            
-        case testTypeDownload:
-            return [[NSString stringWithFormat:@"0:0:%i:%i:%@", numberOfPackets, size, [socketCtrl localHost]] dataUsingEncoding:NSUTF8StringEncoding];
-            break;
-            
-        default:
-            break;
+    if (currentTest == testTypeUpload) {
+        return [[NSString stringWithFormat:@"0:1:%i:%i:%@", numberOfPackets, size, [socketCtrl localHost]] dataUsingEncoding:NSUTF8StringEncoding];
     }
     
+    else if (currentTest == testTypeDownload) {
+        return [[NSString stringWithFormat:@"0:0:%i:%i:%@", numberOfPackets, size, [socketCtrl localHost]] dataUsingEncoding:NSUTF8StringEncoding];
+    }
+    
+    return nil;
+    
+}
+
+-(NSArray *)makeUploadTestPacket{
+    
+    NSMutableArray *dataPacket = [[NSMutableArray alloc] init];
+    
+    for (int i = 1; i <= numberOfTestPackets; i++) {
+
+        NSMutableString *dataStr = [NSMutableString stringWithFormat:@"3:%d:%@:%d:", sizeOfTestPackets, [socketCtrl localHost], i];
+        for (int j = (int)dataStr.length; j < sizeOfTestPackets; j++) {
+            [dataStr appendString:@"1"];
+        }
+        NSLog(@"new string is:%@", dataStr);
+        [dataPacket addObject:[dataStr dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+    return dataPacket;
 }
 
 #pragma mark - TCP socket
 
 -(void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port {
-    
+    NSLog(@"%@",[socketCtrl localHost]);
     NSLog(@"TCP socket connected to - %@:%i", host, port);
     if (currentTest == testTypeUpload) {
-        // send test request
+        // send control packet
         NSData *controlPacket = [self makeControlPacketWithNumberOfPackets:10 packetSize:996];
+        dataToSend = [self makeUploadTestPacket];
         [socketCtrl writeData:controlPacket withTimeout:-1 tag:0];
         
         //wait for response
@@ -105,6 +119,18 @@
     
     NSString *response = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     NSLog(@"TCP data read = %@", response);
+    
+    if (!isWaitingForResults) {
+        for (int i = 0; i < numberOfTestPackets; i++) {
+            [socketUpload sendData:[dataToSend objectAtIndex:i] toHost:serverIP port:serverUploadPort withTimeout:-1 tag:0];
+        }
+        
+        [socketCtrl readDataWithTimeout:-1 tag:0];
+        
+        isWaitingForResults = YES;
+    } else {
+        isWaitingForResults = NO;
+    }
 }
 
 -(void)socket:(GCDAsyncSocket *)sock didWriteDataWithTag:(long)tag {
@@ -114,5 +140,31 @@
 -(void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err {
     NSLog(@"TCP socket disconnected");
 }
+
+
+
+#pragma mark - UDP sockets
+
+-(void)udpSocket:(GCDAsyncUdpSocket *)sock didReceiveData:(NSData *)data fromAddress:(NSData *)address withFilterContext:(id)filterContext {
+    
+    double interval = [startTime timeIntervalSinceNow];
+    NSString *readData = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"UDP data read = %@", readData);
+    NSLog(@"UDP recv time stamp = %f", interval);
+    
+    [recvData addObject:readData];
+    [recvTimeStamps addObject:[NSNumber numberWithDouble:interval]];
+}
+
+-(void)udpSocket:(GCDAsyncUdpSocket *)sock didSendDataWithTag:(long)tag {
+    
+    NSLog(@"Data sent to UPD socket");
+    
+}
+
+-(void)udpSocket:(GCDAsyncUdpSocket *)sock didConnectToAddress:(NSData *)address {
+    NSLog(@"Connected to %@", address);
+}
+
 
 @end
